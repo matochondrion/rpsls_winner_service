9c3d0f2d3063eb1cae45722bd91fff7dff94fac7
matochondrion <matochondrion@gmail.com>
2019-09-02 21:41:24 +0000

    add valid_choice check endpoint
    - check if a choice is valid

--- app.rb
+++ app.rb
@@ -6,33 +6,33 @@ require 'http'
 
 CHOICES = [
   {
-    'choice': {
-      'id': 1,
-      'name': 'rock'
+    choice: {
+      id: 1,
+      name: 'rock'
     }
   },
   {
-    'choice': {
-      'id': 2,
-      'name': 'paper'
+    choice: {
+      id: 2,
+      name: 'paper'
     }
   },
   {
-    'choice': {
-      'id': 3,
-      'name': 'scissors'
+    choice: {
+      id: 3,
+      name: 'scissors'
     }
   },
   {
-    'choice': {
-      'id': 4,
-      'name': 'lizard'
+    choice: {
+      id: 4,
+      name: 'lizard'
     }
   },
   {
-    'choice': {
-      'id': 5,
-      'name': 'spock'
+    choice: {
+      id: 5,
+      name: 'spock'
     }
   }
 ]
@@ -48,6 +48,14 @@ end
 def get_winner(player_choice_id, computer_choice_id)
 end
 
+def valid_choice? choice
+  valid_ids = CHOICES.reduce [] do |memo, choice|
+    memo.push(choice[:choice][:id])
+  end
+
+  valid_ids.include? choice
+end
+
 get '/' do
   'tie'
 end
@@ -61,4 +69,18 @@ end
 get '/choices' do
   content_type :json
   CHOICES.to_json
+end
+
+post '/is_valid_choice' do
+  content_type :json
+
+  request.body.rewind  # in case someone already read it
+  data = JSON.parse request.body.read
+
+  is_valid = valid_choice? data['choice_id'].to_i
+  puts '=> choice_id: ' + data['choice_id'].to_s
+
+  {
+    valid: is_valid
+  }.to_json
 end
